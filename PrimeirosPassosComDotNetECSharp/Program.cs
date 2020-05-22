3390cbaa14829cd37d7059ab20078b8f9c5118a1
Vinicius Aguiar <aguiar.santos.vinicius@gmail.com>
2020-05-22 19:19:19 -0300

    Estrutra de programa CSharp

--- PrimeirosPassosComDotNetECSharp/Program.cs
+++ PrimeirosPassosComDotNetECSharp/Program.cs
@@ -6,7 +6,19 @@ namespace PrimeirosPassosComDotNetECSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // int numeroDeVezes = 10;
+            // for (int i = 0; i < numeroDeVezes; i++)
+            // {
+            //     Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+            // }
+
+            var pilha = new Pilha();
+            pilha.Empilha(1);
+            pilha.Empilha(10);
+            pilha.Empilha(100);
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
         }
     }
 }
